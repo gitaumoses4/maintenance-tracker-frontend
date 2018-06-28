70b6f0cc4a031470bfc6c962b790f0eec018350c
Moses Gitau <gitaumoses4@gmail.com>
2018-06-29 01:18:06 +0300

    [Chore #158708485] Add latest feedback component

---
+++ js/components/LatestFeedback.js
@@ -0,0 +1,40 @@
+import WebComponent from "./WebComponent.js";
+
+export default class LatestFeedback extends WebComponent {
+    constructor() {
+        super("user-home-feedback", "GET", API_BASE_URL + '/users/feedback', getAuthHeaders());
+    }
+
+    render() {
+        return "";
+    }
+
+    success() {
+        super.success();
+        let data = this.data;
+        if (data.data.length === 0) {
+            this.element.innerHTML = `
+            <div  class="empty">
+                No feedback yet <br/>
+                <i class="fas fa-database"></i>
+            </div>`
+        } else {
+            this.element.innerHTML =
+                `${data.data.map(feed => `
+                    <a class="feedback" href="request.html?id=${ feed.feedback.request }">
+                        <img src="../images/user-male.png" alt="" class="mg tiny circle image">
+                        <div class="content">
+                            <div class="title">
+                                ${ feed.created_by.firstname } ${ feed.created_by.lastname }
+                            </div>
+                            <div class="date">
+                                ${ feed.feedback.updated_at }
+                            </div>
+                            <div class="description">
+                                ${ feed.feedback.message }
+                            </div>
+                        </div>
+                    </a>`)}`
+        }
+    }
+}
