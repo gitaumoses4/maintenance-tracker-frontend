754c63cde411d6e518aae045bf0189ff2c89d117
Moses Gitau <gitaumoses4@gmail.com>
2018-07-02 18:45:27 +0300

    Add dynamic port for heroku

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
-connect().use(serveStatic(__dirname+"")).listen(config.port, function () {
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8000;
+var server_host = process.env.YOUR_HOST || '127.0.0.1';
+
+connect().use(serveStatic(__dirname+"")).listen(server_port, server_host, function () {
     console.log("Server running...");
 });
