462b694b7b0d55e619463eec1813faf2a0a8a6f6
Moses Gitau <gitaumoses4@gmail.com>
2018-07-03 14:34:48 +0300

    [Feature #158786666] Add ability to dynamically create sidebar menu

--- js/pages/App.js
+++ js/pages/App.js
@@ -9,22 +9,58 @@ export default class App extends Component {
         return ``;
     }
 
+    getSidebarActiveItem() {
+        return 0;
+    }
+
+    getSideBarMenuItems() {
+        return []
+    }
+
     sideBarMenu() {
-        return ``;
+        let items = this.getSideBarMenuItems();
+        if (items.length === 0) {
+            return "";
+        } else {
+            let menu = document.createElement("div");
+            menu.classList.add("menu");
+            items.map((item, index) => {
+                let menuItem = document.createElement("a");
+                menuItem.classList.add("item");
+                if (index === this.getSidebarActiveItem()) {
+                    menuItem.classList.add("active");
+                }
+                if (item.hasOwnProperty("href")) {
+                    menuItem.setAttribute("href", item.href);
+                }
+                if (item.hasOwnProperty("id")) {
+                    menuItem.id = item.id;
+                }
+                menuItem.innerHTML = `
+                    <i class="icon fas ${ item.icon }"></i>
+                    <div class="title center aligned">
+                        <div class="title">${ item.title }</div>
+                    </div>`;
+                menu.appendChild(menuItem);
+            });
+            return menu.outerHTML;
+        }
+    }
+
+    hasSideBar() {
+        return this.getSideBarMenuItems().length !== 0;
     }
 
     getSideBar() {
         return `<div class="mg sidebar" data-trigger="openSidebar">
-                    <div class="menu">
                         ${ this.sideBarMenu() }
-                    </div>               
                 </div>`
     }
 
     navBar() {
         return `<div class="mg navbar">
-                    ${ this.sideBarMenu() ? ` <i class="open fas fa-bars" id="openSidebar"></i>` : ""}
-                    <div class="header padded left">${ this.getTitle() }</div>
+                    ${ this.hasSideBar() ? ` <i class="open fas fa-bars" id="openSidebar"></i>` : ""}
+                    <div class="header ${ this.hasSideBar() ? `` : "padded" } left">${ this.getTitle() }</div>
                     <div class="menu">
                         ${ this.navBarMenu() } 
                     </div>
@@ -41,7 +77,7 @@ export default class App extends Component {
 
     render() {
         return `
-            ${ this.sideBarMenu() ? this.getSideBar() : ""}
+            ${ this.hasSideBar() ? this.getSideBar() : ""}
             <div class="mg pushable">
                 ${ this.navBar() }
                 <div class="mg content">
@@ -49,5 +85,16 @@ export default class App extends Component {
                 </div>
             </div> 
         `;
+    }
+
+    onRender() {
+        if (this.sideBarMenu()) {
+            initSidebar(this.element.querySelector(".mg.sidebar"))
+        }
+        this.registerComponents();
+    }
+
+    registerComponents() {
+
     }
 }
