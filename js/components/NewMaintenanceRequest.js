5f107c679cfc1107815300fdbd6d6d35658f3e5c
Moses Gitau <gitaumoses4@gmail.com>
2018-07-03 15:14:01 +0300

    [Feature #158786666] Add new request page

--- js/components/NewMaintenanceRequest.js
+++ js/components/NewMaintenanceRequest.js
@@ -15,54 +15,6 @@ export default class NewMaintenanceRequest extends FormComponent {
         super.success();
     }
 
-    render() {
-        return `
-             <div class="status">
-                <div class="mg segment error">
-                </div>
-                <div class="mg segment success">
-                </div>
-            </div>
-            <div class="field">
-                <label>Photo</label>
-                <div class="fields">
-                    <div class="four-large four-medium six-small field">
-                        <img src="../images/placeholder.png" class="mg image" id="product-photo-img"
-                             style="width: 100%"/>
-                    </div>
-                    <div class="eight-large eight-medium six-small field">
-                        <label for="product-photo">Choose Photo
-                            <input type="file" size="10" id="product-photo" required
-                                   accept="image/png, image/jpeg"/>
-                            <input type="hidden" name="photo" id="product-photo-input">
-                        </label>
-                        <button class="mg danger button"
-                                type="button" id="product-photo-remove">Remove
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div class="field">
-                <label>Product Name</label>
-                <input type="text" name="product_name" id="new-request-product-name" required
-                       placeholder="Product Name"/>
-            </div>
-            <div class="field">
-                <label>Description</label>
-                <textarea rows="10" name="description" required id="new-request-description"></textarea>
-            </div>
-            <div class="field">
-                <div class="fields">
-                    <div class="field">
-                        <button class="mg button" id="cancel-request" type="reset">Clear</button>
-                    </div>
-                    <div class="field">
-                        <button class="mg primary button" id="submit-request" type="button">Submit</button>
-                    </div>
-                </div>
-            </div>
-        `
-    }
 
     onRender() {
         let product_photo = this.element.querySelector("#product-photo");
@@ -87,7 +39,7 @@ export default class NewMaintenanceRequest extends FormComponent {
 
         remove_photo.onclick = function () {
             product_photo.value = '';
-            product_photo_img.setAttribute("src", '../images/placeholder.img');
+            product_photo_img.setAttribute("src", '../images/placeholder.png');
         };
 
         let that = this;
@@ -105,7 +57,7 @@ export default class NewMaintenanceRequest extends FormComponent {
                 .then(data => {
                     if (data.error) {
                         that.data = {"status": "error", "message": ["Error uploading maintenance/request photo"]};
-                        that.success();
+                        that.error();
                     } else if (data.secure_url !== '') {
                         that.element.classList.remove("loading");
 
