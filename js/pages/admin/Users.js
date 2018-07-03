43dffc82ab6eceebd4f5378328a292a2195a3218
Moses Gitau <gitaumoses4@gmail.com>
2018-07-03 22:12:43 +0300

    [Feature #158786666] Add admin's list for all users

---
+++ js/pages/admin/Users.js
@@ -0,0 +1,19 @@
+import {AdminDashboard} from "./AdminDashboard.js";
+import AllUsers from "../../components/AllUsers.js";
+
+export class Users extends AdminDashboard{
+   constructor(){
+       super();
+   }
+
+   mainContent(){
+      return `<div id="admin-view-all-users"> </div>`
+   }
+
+   registerComponents(){
+      const allUsers = new AllUsers("admin-view-all-users");
+      allUsers.load();
+   }
+}
+
+new Users();
