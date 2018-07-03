174a912195949fbea8bc53c6c15df22fdd87d933
Moses Gitau <gitaumoses4@gmail.com>
2018-07-03 18:26:24 +0300

    [Feature #158786666] Add admin's notifications

--- js/components/NotificationItem.js
+++ js/components/NotificationItem.js
@@ -11,13 +11,14 @@ export default class NotificationItem extends Component {
         this.element.addEventListener("click", function () {
             let notification = that.data;
             if (that.data.id) {
+                let actions = {"upgrade": "/admin", "signup": "/admin/users.html"};
                 fetch(API_BASE_URL + "/users/notifications/" + that.data.id, {
                     method: "PUT",
                     headers: getAuthHeaders()
                 }).then(response => response.json())
                     .then(data => {
                         that.parent.load();
-                        window.location.href = notification.action === "upgrade" ? "/admin" : "request.html?id=" + notification.action;
+                        window.location.href = actions.hasOwnProperty(notification.action) ? actions[notification.action] : (isAdmin() ? "/admin/": "/user/") +"request.html?id=" + notification.action;
                     })
             }
         });
