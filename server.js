2fd4636fb61b3d7be0b7d4b15e72e0a7d80468d9
Moses Gitau <gitaumoses4@gmail.com>
2018-07-09 08:41:50 +0300

    Change port to 5000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
-var server_port = process.env.PORT || 8000;
+var server_port = process.env.PORT || 5000;
 
 connect().use(serveStatic(__dirname+"")).listen(server_port, function () {
     console.log("Server running...");
