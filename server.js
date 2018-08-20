7f95041bd3122e9ab6e6c3becf400a75b3bb0cae
Moses Gitau <gitaumoses4@gmail.com>
2018-08-20 12:00:19 +0300

    Initializa mocha

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
-var server_port = process.env.PORT || 5000;
+var server_port = process.env.PORT || 7000;
 
 connect().use(serveStatic(__dirname+"")).listen(server_port, function () {
     console.log("Server running...");
