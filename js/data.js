51e4d76c859a89585efde678ebf7eafa6e206aa0
Moses Gitau <gitaumoses4@gmail.com>
2018-08-24 14:59:45 +0300

    Fix issue with getting the date

--- js/data.js
+++ js/data.js
@@ -124,7 +124,7 @@ function prettyDate(time) {
         day_diff = Math.floor(diff / 86400);
 
     if (isNaN(day_diff) || day_diff < 0 || day_diff >= 31) {
-        return;
+        return time;
     }
 
     return day_diff === 0 &&
